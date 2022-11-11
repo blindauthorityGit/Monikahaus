59f7c448038acc366b0bcf444323c941fd661132
Johannes <office@atelierbuchner.at>
2022-11-11 19:34:28 +0100

    mobile Spende

--- components/modalContent/tierheimContent.jsx
+++ components/modalContent/tierheimContent.jsx
@@ -1,6 +1,20 @@
 import React, { useState, useEffect, useContext } from "react";
 import MainContainer from "../layout/mainContainer";
 import Logo from "../../assets/Logo ganz neu_small.jpg";
+import Leitung from "../../assets/leitung.jpg";
+import Schaeferhund from "../../assets/schaeferhund.jpg";
+import Kater from "../../assets/kater.jpg";
+import Kaninchen from "../../assets/kaninchen.jpg";
+
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
 
 const TierheimContent = () => {
     return (
@@ -22,7 +36,28 @@ const TierheimContent = () => {
                         wird, hiervon können die <strong>lfd. Kosten des Tierheims</strong> zu ca. zwei Dritteln gedeckt
                         werden. Für das fehlende Drittel ist das Tierheim auf Spenden, Patenschaften, etc. angewiesen.
                     </p>
-                    <p>
+                    <Swiper
+                        // install Swiper modules
+                        modules={[Navigation, Pagination, Scrollbar, A11y]}
+                        spaceBetween={50}
+                        slidesPerView={1}
+                        navigation
+                        pagination={{ clickable: true }}
+                        scrollbar={{ draggable: true }}
+                        onSwiper={(swiper) => console.log(swiper)}
+                        onSlideChange={() => console.log("slide change")}
+                    >
+                        <SwiperSlide>
+                            <img src={Schaeferhund.src} alt="" />
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <img src={Kater.src} alt="" />
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <img src={Kaninchen.src} alt="" />
+                        </SwiperSlide>
+                    </Swiper>
+                    <p className="mt-4">
                         Im Tierheim Dreieich werden Tiere untergebracht und versorgt, die im Gebiet der
                         Mitgliedsgemeinden aufgefunden werden oder betreuungsbedürftig geworden sind (Fund- und
                         Abgabetiere). Sofern die <strong>Platzverhältnisse</strong> es erlauben, werden auch
@@ -48,6 +83,7 @@ const TierheimContent = () => {
                         Namen der Tiere, sehr dankbar.
                     </p>
                 </div>
+                <img src={Leitung.src} alt="" />
             </div>
         </MainContainer>
     );
