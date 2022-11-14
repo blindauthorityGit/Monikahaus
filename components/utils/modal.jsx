ebccfe2c1f4f14fb561ffc33c383c5e2f1aa3f7c
Johannes <office@atelierbuchner.at>
2022-11-14 12:00:40 +0100

    modal height

--- components/utils/modal.jsx
+++ components/utils/modal.jsx
@@ -15,7 +15,7 @@ const Modal = (props) => {
             }}
             animate={{ x: isMobile ? 0 : 20.14, y: props.closeMe ? 700 : 20.685, scale: 1, rotate: 0 }}
             transition={{ duration: "30ms", delay: props.delay, type: "spring" }}
-            className={`absolute sm:fixed h-2/4 bottom-6 sm:bottom-auto  sm:h-full max-h-[100%] fade-in w-full sm:w-[90%] lg:w-[45%] sm:min-h-[100%] bg-white p-8 lg:p-24 z-50 `}
+            className={`absolute sm:fixed h-[60%] bottom-6 sm:bottom-auto  sm:h-full max-h-[100%] fade-in w-full sm:w-[90%] lg:w-[45%] sm:min-h-[100%] bg-white p-8 lg:p-24 z-50 `}
         >
             <div
                 className="closer absolute top-6 right-6 text-4xl cursor-pointer transition hover:opacity-50 z-50"
